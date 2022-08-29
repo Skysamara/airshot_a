9c7cca97e5a1b4c6fafbb03b45bfb2972ea7a6c1
skysamara <skysamara@gmail.com>
2022-08-29 21:30:23 +0300

    onClick

--- app/src/main/java/ru/panorobot/airshot/MainActivity.kt
+++ app/src/main/java/ru/panorobot/airshot/MainActivity.kt
@@ -1,13 +1,96 @@
 package ru.panorobot.airshot
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+// Почему не получалось обратиться к элементам формы напрямую
+// https://stackoverflow.com/questions/52271521/import-kotlinx-android-synthetic-main-activity-main-is-not-working
+//
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_CODE_ENABLE_BT:Int = 1
+
+    // bluetooth adapter
+    lateinit var bAdapter:BluetoothAdapter
+
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-        val draw2D = Draw2D(this)
-        setContentView(draw2D)
+        setContentView(R.layout.activity_main)
+
+        // bluetooth adapter
+        bAdapter = BluetoothAdapter.getDefaultAdapter()
+        // check is on/off
+        if(bAdapter == null){
+            bluetoothStatusTV.text = "Bluetooht is not available"
+        }
+        else{
+            bluetoothStatusTV.text = "Bluetooht is available"
+        }
+        // Статус bluetooth
+        if (bAdapter.isEnabled){
+            bluetoothIv.setImageResource(R.drawable.ic_bluetooth_on)
+        }
+        else{
+            bluetoothIv.setImageResource(R.drawable.ic_bluetooth_off)
+        }
+
+        // Включаем bluetooth
+        turnOnBtn.setOnClickListener {
+            if (bAdapter.isEnabled){
+                // Уже включен
+                Toast.makeText(this, "Already on", Toast.LENGTH_LONG).show()
+            }
+            else{
+                // Включаем
+                var intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(intent, REQUEST_CODE_ENABLE_BT)
+            }
+        }
+        // Выключаем bluetooth
+        turnOffBtn.setOnClickListener {
+
+        }
+        // Ищем bluetooth
+        discoverableBtn.setOnClickListener {
+
+        }
+
+
+
+
+
+
+
+ // ******************************************       startShooting()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when(requestCode){
+            REQUEST_CODE_ENABLE_BT ->
+                if (requestCode == Activity.RESULT_OK){
+                    bluetoothIv.setImageResource(R.drawable.ic_bluetooth_on) // 14:14
+                    Toast.makeText(this, "Bluetooth is on", Toast.LENGTH_LONG).show()
+                }
+            else{
+                    Toast.makeText(this, "Could not on bluetooth", Toast.LENGTH_LONG).show()
+                }
+
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun startShooting() {
+        val target = Target(this)
+        target.setOnClickListener {
+            target.invalidate()
+        }
+        setContentView(target)
     }
 }
---
+++ app/src/main/java/ru/panorobot/airshot/Target.kt
@@ -0,0 +1,73 @@
+//https://developer.alexanderklimov.ru/android/simplepaint.php
+
+package ru.panorobot.airshot
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.View
+
+class Target(context: Context?) : View(context) {
+
+    private val paint: Paint = Paint()
+    private val shots:Shots = Shots()
+
+//    override fun setOnTouchListener(l: OnTouchListener?) {
+////        super.setOnTouchListener(l)
+////        invalidate()
+//    }
+//
+//    override fun setOnClickListener(l: OnClickListener?) {
+////        super.setOnClickListener(l)
+////        invalidate()
+//    }
+
+
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        val screenWidth: Int = getWidth()
+        val screenHeight: Int = getHeight()
+
+        drawTarget(canvas, screenWidth, screenHeight)
+        shots.draw(canvas,screenWidth, screenHeight)
+    }
+
+
+    private fun drawTarget(canvas: Canvas?, screenWidth: Int, screenHeight: Int) {
+        paint.apply {
+            style = Paint.Style.FILL // стиль Заливка
+            color = Color.WHITE // закрашиваем холст белым цветом
+        }
+        canvas?.drawPaint(paint)
+
+        paint.apply {
+            isAntiAlias = true
+            color = Color.BLACK
+        }
+
+        val centerX = width / 2
+        val centerY = height / 2
+
+        var radiusApple: Int = (screenWidth + screenHeight) / 30
+        canvas!!.drawCircle(centerX.toFloat(), centerY.toFloat(), radiusApple.toFloat(), paint)
+
+        paint.apply {
+            style = Paint.Style.STROKE
+        }
+        var radius = radiusApple * 2
+        canvas.drawCircle(centerX.toFloat(), centerY.toFloat(), radius.toFloat(), paint)
+
+        radius = radiusApple * 3
+        canvas.drawCircle(centerX.toFloat(), centerY.toFloat(), radius.toFloat(), paint)
+
+        radius = radiusApple * 4
+        canvas.drawCircle(centerX.toFloat(), centerY.toFloat(), radius.toFloat(), paint)
+
+        radius = radiusApple * 5
+        canvas.drawCircle(centerX.toFloat(), centerY.toFloat(), radius.toFloat(), paint)
+    }
+
+
+}
